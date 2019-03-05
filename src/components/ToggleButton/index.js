9bc34d767fd1d404cb34d9ff600b5b06a2d01f7c
Todd Seller <me@toddseller.com>
2019-03-05 05:49:59 -0800

    Start ToggleButton component refactor

---
+++ src/components/ToggleButton/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import isBoolean from 'lodash/isBoolean'
+import isFunction from 'lodash/isFunction'
+
+import './index.css'
+import { isString } from '@material-ui/core/es/styles/transitions'
+
+class ToggleButton extends Component {
+  state = { selected: this.selectedFromProps() }
+
+  isSelected = () => this.state.selected
+
+  selectedFromProps() {
+    let { selected } = this.props
+
+    selected = isFunction(selected) ? selected() : selected
+
+    return isBoolean(selected) && selected
+  }
+
+  toggleButton = e => {
+    e.persist()
+    e.preventDefault()
+    console.log(e)
+    const { onClick, onStateChanged } = this.props
+
+    this.setState({ selected: !this.state.selected }, () => {
+      const state = this.state
+      const buttonEvent = Object.assign(e, { BUTTON_STATE: state })
+
+      isFunction(onClick) && onClick(buttonEvent)
+      isFunction(onStateChanged) && onStateChanged(state)
+    })
+  }
+
+  render() {
+    const borderStyles = {
+      deselected: {
+        borderTop: 'none #e50914',
+        borderLeft: 'none #e50914',
+        borderRight: 'none #e50914',
+        borderBottom: 'solid 3px #e50914',
+        bottom: '-5px',
+        boxSizing: 'content-box',
+        margin: '0 auto',
+        position: 'relative',
+        width: 'calc(100% - 1.5rem)',
+        transform: 'scaleX(0)',
+        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+      },
+      selected: {
+        borderTop: 'none #e50914',
+        borderLeft: 'none #e50914',
+        borderRight: 'none #e50914',
+        borderBottom: 'solid 3px #e50914',
+        bottom: '-5px',
+        boxSizing: 'content-box',
+        margin: '0 auto',
+        position: 'relative',
+        width: 'calc(100% - 1.5rem)',
+        transform: 'scaleX(1)',
+        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+      }
+    }
+    const { selected } = this.state
+    const { selected: _selected, theme, onClick, className, onStateChanged, ...restProps } = this.props
+    const buttonTheme = (theme && isString(theme)) ? theme : 'default'
+    const buttonClasses = classnames(
+      `button button--${ buttonTheme }`,
+      className
+    )
+    let borderBottomStyles = selected ? borderStyles.selected : borderStyles.deselected
+    // const borderClasses = classnames(
+    //   `button--${ selected ? 'selected' : 'deselected' }`,
+    // )
+
+    return (
+      <div>
+        <div className={ buttonClasses } onClick={ this.toggleButton } { ...restProps }></div>
+        <div>
+          <hr style={ borderBottomStyles } />
+        </div>
+      </div>
+    )
+  }
+}
+
+ToggleButton.propTypes = {
+  theme: PropTypes.string,
+  selected: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.func
+  ]),
+  onStateChanged: PropTypes.func
+}
+
+export default ToggleButton
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import compose from 'recompose/compose'
 import { forceCheck } from 'react-lazyload'
+import snakeCase from 'lodash/snakeCase'
+import classnames from 'classnames'
 
 import { startFetchMovies } from '../actions/userMovie'
 import { setTextFilter, setDisplayAll, setDisplayUnwatched } from '../actions/filters'
 import FilteredMoviesSelector from '../selectors/filteredMovies'
 import LoadingPage from './LoadingPage'
 import MoviePreview from './MoviePreview'
+import ToggleButton from './ToggleButton'
+
 import '../styles/movie-preview.css'
 
+const DISPLAY_FILTER = []
+
 class UserPage extends Component {
+  state = { selected: true, only: DISPLAY_FILTER.map(snakeCase) }
+
+  toggleSelectedButton = display => ({selected}) => {
+    let { only } = this.state
+
+    if (selected && !only.includes(display)) {
+      only.push(display)
+      return this.setState({ only })
+    }
+
+    if (!selected && only.includes(display)) {
+      only = only.filter(item => item !== display)
+      console.log(only)
+      return this.setState({ only })
+    }
+  }
+
   componentDidMount() {
     this.props.startFetchMovies()
     return <LoadingPage />
@@ -30,18 +53,34 @@ class UserPage extends Component {
 
   render() {
     const { movies } = this.props
+    const { selected } = this.state
     if (movies.length < 1) {
       return <LoadingPage />
     }
     return (
       <div>
         <div className='display-by'>
-          <div className="display-all" onClick={ this.setDisplayAll }>
+          {/*<div className="display-all" onClick={ this.setDisplayAll }>*/ }
+          {/*Library*/ }
+          {/*</div>*/ }
+          {/*<div className="display-unwatched" onClick={ this.setDisplayUnwatched }>*/ }
+          {/*Unwatched*/ }
+          {/*</div>*/ }
+          <ToggleButton
+            className="display-all"
+            selected={ selected }
+            onStateChanged={ this.toggleSelectedButton(snakeCase('Library')) }
+            onClick={ this.setDisplayAll }
+          >
             Library
-          </div>
-          <div className="display-unwatched" onClick={ this.setDisplayUnwatched }>
+          </ToggleButton>
+          <ToggleButton
+            className="display-unwatched"
+            onStateChanged={ this.toggleSelectedButton(snakeCase('Unwatched')) }
+            onClick={ this.setDisplayUnwatched }
+          >
             Unwatched
-          </div>
+          </ToggleButton>
         </div>
         <div className="movies-list">
           {
