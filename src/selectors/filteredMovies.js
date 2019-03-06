ec0a638ef0bf35f211b78057d648c3e65a6b78b9
Todd Seller <me@toddseller.com>
2019-03-05 20:08:18 -0800

    Remove console.log

--- src/selectors/filteredMovies.js
+++ src/selectors/filteredMovies.js
@@ -5,7 +5,6 @@ const moviesSelector = state => state.movies
 const titleSelector = state => state.filters
 
 const getMovies = (movies, filters) => {
-  console.log(movies[0])
   const filteredMovies = _.filter(
     movies,
     movie => filters.display === 'isNew' ? movie.search_name.includes(filters.text.toLowerCase()) && movie.isNew : movie.search_name.includes(filters.text.toLowerCase())
